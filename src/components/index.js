6fb20944c37acdad5a615f459a7771ac75d23c00
unknown <abdehiperdana@gmail.com>
2022-11-05 09:43:51 +0800

    website aplikasi kasir

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import NavbarComponent from './NavbarComponent';
+import ListCategories from './ListCategories';
+import Hasil from './hasil';
+import Menus from './Menus';
+
+export {Hasil, ListCategories, NavbarComponent, Menus}
